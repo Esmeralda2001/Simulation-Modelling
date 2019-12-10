c313dff4f2b3f1d1161500eb168b2f864544189e
Esmeralda <esmeralda.dewolf@student.hu.nl>
2019-12-10 17:00:34 +0100

    added graph pictures

--- Week1/plotsFalling.py
+++ Week1/plotsFalling.py
@@ -29,16 +29,38 @@ def forward_euler():
         # schrijf hier code voor Euler-integratie
         t[step+1] = (step + 1) * h
         x[step + 1] = x[step] + v[step]*h
-        #v[step+1] = spring_friction(h, g, x, v, step, stretch, friction)
+        v[step+1] = spring_friction(h, g, x, v, step, stretch, friction)
         #v[step+1] = spring(h, g, x, v, step, stretch)
-        v[step + 1] = falling_friction(h, g, v, step, friction)
+        #v[step + 1] = falling_friction(h, g, v, step, friction)
         #v[step + 1] = falling(h, g, v, step)
 
     return t,x,v
 
+def forward_euler_springfriction():
+    h=0.1
+    g=9.81
+    friction = 0.1
+    stretch = 4
+
+    num_steps = 50
+    t = numpy.zeros(num_steps+1)
+    x = numpy.zeros(num_steps+1)
+    v = numpy.zeros(num_steps+1)
+
+
+    for step in range(num_steps):
+        # schrijf hier code voor Euler-integratie
+        t[step+1] = (step + 1) * h
+        x[step + 1] = x[step] + v[step]*h
+        v[step+1] = spring_friction(h, g, x, v, step, stretch, friction)
+
+    return t,x,v
+
+
+
 t,x,v = forward_euler()
 
-def plot_me():
+def plot_me(t, x, v):
     axes_height = matplotlib.pyplot.subplot(211)
     matplotlib.pyplot.plot(t, x)
     axes_velocity = matplotlib.pyplot.subplot(212)
@@ -46,6 +68,6 @@ def plot_me():
     axes_height.set_ylabel('Height in m')
     axes_velocity.set_ylabel('Velocity in m/s')
     axes_velocity.set_xlabel('Time in s')
-    matplotlib.pyplot.show()
 
-plot_me()
+plot_me(t, x, v)
+matplotlib.pyplot.show()
